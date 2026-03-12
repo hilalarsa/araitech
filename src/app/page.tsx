7e1130377cd2455ec375f8c540fce80e342cc7cf
Hilal Arsa <hilalarsa@gmail.com>
2026-03-12 11:01:25 +0700

    Remove testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Services from "@/components/Services";
 import Portfolio from "@/components/Portfolio";
-import Testimonials from "@/components/Testimonials";
 import CTA from "@/components/CTA";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
@@ -16,7 +15,6 @@ export default function Home() {
       <About />
       <Services />
       <Portfolio />
-      {/* <Testimonials /> */}
       <CTA />
       <Contact />
       <Footer />
